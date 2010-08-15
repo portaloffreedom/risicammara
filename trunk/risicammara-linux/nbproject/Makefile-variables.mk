7e103d9a821648cb96f27b269d6712ebdc0090c2
Robenfi <Robenfi@fbff0255-8af7-72cb-3705-11769a256df1>
2010-08-15 17:53:28 +0000

    transizione

--- trunk/risicammara-linux/nbproject/Makefile-variables.mk
+++ trunk/risicammara-linux/nbproject/Makefile-variables.mk
@@ -14,6 +14,14 @@ CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Windows/risicammara-linux
 CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Windows/package
 CND_PACKAGE_NAME_Debug=risicammara-linux.tar
 CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Windows/package/risicammara-linux.tar
+# Debug_ROB configuration
+CND_PLATFORM_Debug_ROB=MinGW-Windows
+CND_ARTIFACT_DIR_Debug_ROB=dist/Debug_ROB/MinGW-Windows
+CND_ARTIFACT_NAME_Debug_ROB=risicammara-linux
+CND_ARTIFACT_PATH_Debug_ROB=dist/Debug_ROB/MinGW-Windows/risicammara-linux
+CND_PACKAGE_DIR_Debug_ROB=dist/Debug_ROB/MinGW-Windows/package
+CND_PACKAGE_NAME_Debug_ROB=risicammara-linux.tar
+CND_PACKAGE_PATH_Debug_ROB=dist/Debug_ROB/MinGW-Windows/package/risicammara-linux.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/GNU-Windows
