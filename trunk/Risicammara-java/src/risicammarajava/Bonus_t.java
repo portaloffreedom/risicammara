67b6a6c4836436c7361a216f622c541b8b9ef130
Robenfi <Robenfi@fbff0255-8af7-72cb-3705-11769a256df1>
2011-01-02 11:28:57 +0000

    (Java) Upgradata la gestione dei bonus (solo enumerato)

--- trunk/Risicammara-java/src/risicammarajava/Bonus_t.java
+++ trunk/Risicammara-java/src/risicammarajava/Bonus_t.java
@@ -5,8 +5,16 @@ package risicammarajava;
  * @author stengun
  */
  public enum Bonus_t {
-        CANNONE,
-        FANTE,
-        CAVALLO,
-        JOLLY;
+        CANNONE (4),
+        FANTE   (6),
+        CAVALLO (8),
+        JOLLY   (12);
+        int numarm;
+        Bonus_t(int numarm){
+            this.numarm=numarm;
+        };
+        int TrisValue(boolean diversi){
+            if(diversi) return 12;
+            return numarm;
+        }
  }
