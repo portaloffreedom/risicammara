223bf3538276f576b0fe16f948571e6409e33815
matteo.dek <matteo.dek@fbff0255-8af7-72cb-3705-11769a256df1>
2011-01-05 23:48:51 +0000

    (Java) - Aggiunta di poteri alla super mucca

--- Risicammara-java/src/risicammarajava/PoteriDellaSuperMucca.java
+++ Risicammara-java/src/risicammarajava/PoteriDellaSuperMucca.java
@@ -13,7 +13,13 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.html.HTMLDocument;
+import risicammarajava.playerManage.Giocatore;
 import risicammarajava.turnManage.Partita;
 
 /**
@@ -23,6 +29,7 @@ import risicammarajava.turnManage.Partita;
 public class PoteriDellaSuperMucca extends JFrame {
 
     private Partita partita;
+    private Giocatore giocatoreSelezionato;
 
     public PoteriDellaSuperMucca(Partita partita) {
         super("Poteri della SuperMucca");
@@ -44,57 +51,65 @@ public class PoteriDellaSuperMucca extends JFrame {
         //**********************************************************************
         //impostazioni di pulsanti e robe varie
         
-        // ComboBox per la selezione del Giocatore
+        // ComboBox per la selezione del Giocatore------------------------------
         JComboBox selezioneGiocatore = new JComboBox();
-        //selezioneGiocatore.addItem("Porcone")
         for (int i=0; i<partita.getListaGiocatori().getSize(); i++){
             selezioneGiocatore.addItem(partita.getListaGiocatori().get(i));
         }
+        this.giocatoreSelezionato=partita.getListaGiocatori().get(1);
         pannello.add(selezioneGiocatore);
 
-        // Label con scritto "Numero Armate"
-        pannello.add(new JLabel("Numero Armate"));
+        // Label con scritto "Numero Armate"------------------------------------
+        //pannello.add(new JLabel("Numero Armate"));
 
-        // Spinner per cambiare il numero di armate
+        // Spinner per cambiare il numero di armate-----------------------------
         JSpinner numeroArmate = new JSpinner();
+        numeroArmate.createToolTip().setTipText("Numero Armate");
         pannello.add(numeroArmate);
 
-        // Testo dell'obbiettivo
-        JTextField obbiettivo = new JTextField("PROOOOOOOVAAAAA!!!!\n a capo");
+        // Testo dell'obbiettivo------------------------------------------------
+        JTextArea obbiettivo = new JTextArea();
+        obbiettivo.setText(giocatoreSelezionato.getObbiettivo().toString());
         obbiettivo.setEditable(false);
         pannello.add(obbiettivo);
 
-        // Selezione Territorio
+        // Selezione Territorio-------------------------------------------------
         JComboBox selezioneTerritorio = new JComboBox();
+        //for (int i=0; i<giocatoreSelezionato.getListaterr().size(); i++){
+        //    selezioneGiocatore.addItem(giocatoreSelezionato.getListaterr().get(i));
+        //}
+        for (territori_t territorio : giocatoreSelezionato.getListaterr()){
+            selezioneGiocatore.addItem(territorio);
+        }
         pannello.add(selezioneTerritorio);
 
-        // Spinner per cambiare il numero di armate nel territorio
+        // Spinner per cambiare il numero di armate nel territorio--------------
         JSpinner numeroArmateTerritorio = new JSpinner();
         pannello.add(numeroArmateTerritorio);
 
-        // lista dei territori del Giocatore selezionato
+        // lista dei territori del Giocatore selezionato------------------------
         JList listaTerrori = new JList();
         pannello.add(listaTerrori);
 
-        // Colore armate del Giocatore selezionato
+        // Colore armate del Giocatore selezionato------------------------------
         JTextField coloreArmate = new JTextField("Colore");
         coloreArmate.setEditable(false);
         pannello.add(coloreArmate);
 
-        // Selezione carte in mano
+        // Selezione carte in mano----------------------------------------------
         JComboBox carteInMano = new JComboBox();
         pannello.add(carteInMano);
 
-        // Dati per carta selezionata
+        // Dati per carta selezionata-------------------------------------------
         JTextField datiCarta = new JTextField("Dati della carta");
         datiCarta.setEditable(false);
         pannello.add(datiCarta);
 
-        // Pulsante dai Carta
+        // Pulsante dai Carta---------------------------------------------------
         JButton bottoneDaiCarta = new JButton("Dai Carta");
         pannello.add(bottoneDaiCarta);
 
-        // Pulsante togli Carta
+        // Pulsante togli Carta-------------------------------------------------
         JButton bottoneTogliCarta = new JButton("Tolgi Carta");
         pannello.add(bottoneTogliCarta);
 
