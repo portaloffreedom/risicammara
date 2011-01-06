b13bacdca853999d83975b9481ec63409e2f59c7
matteo.dek <matteo.dek@fbff0255-8af7-72cb-3705-11769a256df1>
2011-01-06 01:24:17 +0000

    (Java) - Aggiunta classe per l'implementazione del testo che va a capo mentre si disegna - Aggiunto bug che fa morire da ridere XD

--- trunk/Risicammara-java/src/PacchettoGrafico/MenuGiocatore.java
+++ trunk/Risicammara-java/src/PacchettoGrafico/MenuGiocatore.java
@@ -8,9 +8,9 @@ package PacchettoGrafico;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import risicammarajava.playerManage.ListaPlayers;
 
 /**
  *
@@ -23,20 +23,19 @@ public class MenuGiocatore extends Elemento_2DGraphics implements ActionListener
     private String obbiettivo;
     private String nomegiocatore;
 
+    /*
     private int larghezza;
     private int altezza;
     private int bordo;
-    private int distanzaSuperiore;
+    private int distanzaSuperiore;*/
+    private Rectangle rettangolo;
     private boolean visibile;
 
     public MenuGiocatore(Dimension dimensioni) {
         super(dimensioni);
         this.visibile=false;
 
-        this.larghezza = 120;
-        this.altezza = 70;
-        this.bordo = 5;
-        this.distanzaSuperiore = 55;
+        this.rettangolo = new Rectangle(5, 55, 120, 70);
     }
 
     @Override
@@ -45,13 +44,17 @@ public class MenuGiocatore extends Elemento_2DGraphics implements ActionListener
 
             //parte provvisoria
             graphics2D.setColor(Color.red);
-            graphics2D.fill3DRect(this.bordo, this.distanzaSuperiore, this.larghezza, this.altezza, true);
+            graphics2D.fill(rettangolo);
             graphics2D.setColor(Color.black);
-            graphics2D.drawString("Tutti gli uomini del presidente", this.bordo+5, this.distanzaSuperiore+15);
+            new TestoACapo(graphics2D, dimensioni, rettangolo, "Tutti gli uomini del presidente").disegna(graphics2D);
+            //graphics2D.drawString("Tutti gli uomini del\n presidente", this.bordo+5, this.distanzaSuperiore+15);
+            
         }
     }
 
     public void actionPerformed(ActionEvent e) {
         this.visibile = !this.visibile;
     }
+    
 }
+
