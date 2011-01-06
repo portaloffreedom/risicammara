908608b099f711ac903b7d2d63277e18aa5b3909
matteo.dek <matteo.dek@fbff0255-8af7-72cb-3705-11769a256df1>
2011-01-06 02:36:10 +0000

    (Java) - Adesso i poteri della SuperMucca posso vedere diverse cose dei giocatori (e non solo del primo giocatore)

--- Risicammara-java/src/risicammarajava/PoteriDellaSuperMucca.java
+++ Risicammara-java/src/risicammarajava/PoteriDellaSuperMucca.java
@@ -6,6 +6,8 @@
 package risicammarajava;
 
 import java.awt.Container;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -31,6 +33,13 @@ public class PoteriDellaSuperMucca extends JFrame {
     private Partita partita;
     private Giocatore giocatoreSelezionato;
 
+    private JPanel pannello;
+    private JComboBox selezioneGiocatore;
+    private JSpinner numeroArmate;
+    private JTextArea obbiettivo;
+    private JComboBox selezioneTerritorio;
+    private JTextField coloreArmate;
+
     public PoteriDellaSuperMucca(Partita partita) {
         super("Poteri della SuperMucca");
         this.partita=partita;
@@ -45,14 +54,14 @@ public class PoteriDellaSuperMucca extends JFrame {
         //**********************************************************************
         //impostazione del pannello
         Container contestoFinestra = this.getContentPane();
-        JPanel pannello = new JPanel();
+        this.pannello = new JPanel();
         contestoFinestra.add(pannello);
 
         //**********************************************************************
         //impostazioni di pulsanti e robe varie
         
         // ComboBox per la selezione del Giocatore------------------------------
-        JComboBox selezioneGiocatore = new JComboBox();
+        this.selezioneGiocatore = new JComboBox();
         for (int i=0; i<partita.getListaGiocatori().getSize(); i++){
             selezioneGiocatore.addItem(partita.getListaGiocatori().get(i));
         }
@@ -63,26 +72,20 @@ public class PoteriDellaSuperMucca extends JFrame {
         //pannello.add(new JLabel("Numero Armate"));
 
         // Spinner per cambiare il numero di armate-----------------------------
-        JSpinner numeroArmate = new JSpinner();
+        this.numeroArmate = new JSpinner();
         numeroArmate.createToolTip().setTipText("Numero Armate");
         pannello.add(numeroArmate);
 
         // Testo dell'obbiettivo------------------------------------------------
-        JTextArea obbiettivo = new JTextArea();
-        obbiettivo.setText(giocatoreSelezionato.getObbiettivo().toString());
+        this.obbiettivo = new JTextArea();
         obbiettivo.setEditable(false);
         pannello.add(obbiettivo);
 
         // Selezione Territorio-------------------------------------------------
-        JComboBox selezioneTerritorio = new JComboBox();
-        //for (int i=0; i<giocatoreSelezionato.getListaterr().size(); i++){
-        //    selezioneGiocatore.addItem(giocatoreSelezionato.getListaterr().get(i));
-        //}
-        for (territori_t territorio : giocatoreSelezionato.getListaterr()){
-            selezioneTerritorio.addItem(territorio);
-        }
+        this.selezioneTerritorio = new JComboBox();
         pannello.add(selezioneTerritorio);
 
+        /*
         // Spinner per cambiare il numero di armate nel territorio--------------
         JSpinner numeroArmateTerritorio = new JSpinner();
         pannello.add(numeroArmateTerritorio);
@@ -90,12 +93,14 @@ public class PoteriDellaSuperMucca extends JFrame {
         // lista dei territori del Giocatore selezionato------------------------
         JList listaTerrori = new JList();
         pannello.add(listaTerrori);
+         */
 
         // Colore armate del Giocatore selezionato------------------------------
-        JTextField coloreArmate = new JTextField("Colore");
+        this.coloreArmate = new JTextField();
         coloreArmate.setEditable(false);
         pannello.add(coloreArmate);
 
+        /*
         // Selezione carte in mano----------------------------------------------
         JComboBox carteInMano = new JComboBox();
         pannello.add(carteInMano);
@@ -112,8 +117,40 @@ public class PoteriDellaSuperMucca extends JFrame {
         // Pulsante togli Carta-------------------------------------------------
         JButton bottoneTogliCarta = new JButton("Tolgi Carta");
         pannello.add(bottoneTogliCarta);
+        */
 
         //**********************************************************************
+
+        cambiaSelezioneGiocatore(giocatoreSelezionato);
+
+        selezioneGiocatore.addItemListener(new ItemListenerImpl(this));
+    }
+
+    final void cambiaSelezioneGiocatore(Giocatore  giocatoreSelezionato){
+        this.giocatoreSelezionato = giocatoreSelezionato;
+
+        obbiettivo.setText(giocatoreSelezionato.getObbiettivo().toString().substring(0, 20));
+
+        selezioneTerritorio.removeAllItems();
+        for (territori_t territorio : giocatoreSelezionato.getListaterr()){
+            selezioneTerritorio.addItem(territorio);
+        }
+
+        coloreArmate.setText(giocatoreSelezionato.getArmyColour().toString());
+        
+    }
+
+    private static class ItemListenerImpl implements ItemListener {
+
+        private PoteriDellaSuperMucca mucca;
+
+        public ItemListenerImpl(PoteriDellaSuperMucca mucca) {
+            this.mucca = mucca;
+        }
+
+        public void itemStateChanged(ItemEvent e) {
+            mucca.cambiaSelezioneGiocatore((Giocatore) e.getItem());
+        }
     }
 
 }
