d2c8a6bcd7ea56cf957842fec1c212dec0432d64
Robenfi <Robenfi@fbff0255-8af7-72cb-3705-11769a256df1>
2011-01-12 05:07:58 +0000

    (Java) Aggiunto un prototipo di lobby (Server side)

--- Risicammara-java/src/risicammaraServer/MessageManage/Messaggio_Comandi.java
+++ Risicammara-java/src/risicammaraServer/MessageManage/Messaggio_Comandi.java
@@ -11,9 +11,17 @@ package risicammaraServer.MessageManage;
  */
 public class Messaggio_Comandi implements Messaggio{
     private comandi_t comando;
+    private String sender;
+    private String receiver;
 
-    public Messaggio_Comandi(comandi_t cmd){
+    public Messaggio_Comandi(comandi_t cmd, String inviante){
+        this(cmd, inviante, null);
+    }
+
+    public Messaggio_Comandi(comandi_t cmd, String inviante, String who){
         this.comando = cmd;
+        this.sender = inviante;
+        this.receiver = who;
     }
 
     public messaggio_t getType() {
@@ -22,5 +30,13 @@ public class Messaggio_Comandi implements Messaggio{
 
     public comandi_t getComando(){
         return comando;
+    }
+
+    public String getSender(){
+        return sender;
+    }
+
+    public String getReceiver(){
+        return receiver;
     }
 }
