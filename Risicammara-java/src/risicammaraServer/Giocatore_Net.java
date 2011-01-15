1d661161e8fbf667c44438c7e61608693e59518e
matteo.dek <matteo.dek@fbff0255-8af7-72cb-3705-11769a256df1>
2011-01-15 02:30:08 +0000

    (java) - (fix) aggiustato il codice di giocatore_net. Gestione errata dei socket. La creazione di milioni di outputstream non porta a corrompere lo stream. Giocatore_net impostato per aprirne uno nel costruttore. Usare getter dello stream (di tipo object-buffered) per utilizzarli

--- Risicammara-java/src/risicammaraServer/Giocatore_Net.java
+++ Risicammara-java/src/risicammaraServer/Giocatore_Net.java
@@ -2,7 +2,15 @@
 
 package risicammaraServer;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import risicammaraClient.Colore_t;
 import risicammaraJava.playerManage.Giocatore;
 
@@ -13,16 +21,33 @@ import risicammaraJava.playerManage.Giocatore;
 public class Giocatore_Net extends Giocatore {
 
     transient private Socket comunicatore;
+    transient private ObjectOutputStream clientOut;
+    transient private ObjectInputStream  clientIn;
     transient private Thread thread_player;
     
     public Giocatore_Net(Socket comunicatore){
         super(null, Colore_t.NULLO);
-        this.comunicatore=comunicatore;
-        thread_player = null;
+        try {
+            this.comunicatore = comunicatore;
+            this.clientIn = new ObjectInputStream(new BufferedInputStream(comunicatore.getInputStream()));
+            this.clientOut = new ObjectOutputStream(new BufferedOutputStream(comunicatore.getOutputStream()));
+            thread_player = null;
+        } catch (IOException ex) {
+            System.err.println("Errore nell'apertura delle comunicazioni con il nuovo giocatore_net creato"+ex.getLocalizedMessage());
+        }
     }
 
+    @Deprecated
     public Giocatore_Net(){
         this(null);
+    }
+
+    public ObjectInputStream getClientIn() {
+        return clientIn;
+    }
+
+    public ObjectOutputStream getClientOut() {
+        return clientOut;
     }
 
     public void AssignThread(Thread t){
@@ -31,11 +56,21 @@ public class Giocatore_Net extends Giocatore {
     public Thread getThread(){
         return thread_player;
     }
+
+    @Deprecated
     public void setSocket(Socket sock){
         this.comunicatore = sock;
     }
+
+    @Deprecated
     public Socket getSocket(){
         return comunicatore;
+    }
+
+    public void closeSocket() throws IOException{
+        this.clientIn.close();
+        this.clientOut.close();
+        this.comunicatore.close();
     }
 
 }
