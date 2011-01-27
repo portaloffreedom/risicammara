8355e9c903565514057ecfb3ba847266dba58017
robenfi <robenfi@fbff0255-8af7-72cb-3705-11769a256df1>
2011-01-27 15:35:10 +0000

    (java) 	Quando tutti i giocatori sono pronti e sono almeno 3 parte la partita. 	Invia il messaggio comando "avviapartita" appena ci sono tutte le cose pronte.

--- trunk/Risicammara-java/src/risicammaraServer/messaggiManage/comandi_t.java
+++ trunk/Risicammara-java/src/risicammaraServer/messaggiManage/comandi_t.java
@@ -17,7 +17,7 @@ public enum comandi_t {
     SETNOPRONTO,     // Informa che il giocatore non è pronto per partire
             
     SETPRONTO,      //Informa che il giocatore è pronto per partire
-    NUOVAPARTITA,   // Ordina al server di iniziare una nuova partita
+    AVVIAPARTITA,   // Ordina al server di iniziare una nuova partita
     KICKPLAYER,     // Espelli Giocatore
     EXIT,           // il giocatore ha disconnesso la lobby
     
