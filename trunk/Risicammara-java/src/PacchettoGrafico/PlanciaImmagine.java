badcd0d111dc1a1c5ac2350366226ce9d70a2154
matteo.dek <matteo.dek@fbff0255-8af7-72cb-3705-11769a256df1>
2011-02-20 23:13:28 +0000

    (Java) - fatta una funzione statica in GraphicsAdvanced che ritorna il colore del testo da usare a seconda dello sfondo - implementato il disegno dei bolli sul territorio - (bug/whish) il centro in cui disegnare il bollino è collocato male, trovare un'altra funzione più adatta per trovare il centro in cui disegnare il bollino.

--- trunk/Risicammara-java/src/PacchettoGrafico/PlanciaImmagine.java
+++ trunk/Risicammara-java/src/PacchettoGrafico/PlanciaImmagine.java
@@ -8,6 +8,7 @@ package PacchettoGrafico;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Rectangle;
@@ -15,8 +16,11 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import risicammaraClient.Colore_t;
 import risicammaraClient.territori_t;
 import risicammaraJava.boardManage.PlanciaClient;
+import risicammaraJava.boardManage.TerritorioPlancia;
+import risicammaraJava.boardManage.TerritorioPlanciaClient;
 import risicammaraJava.turnManage.PartitaClient;
  
 /**
@@ -24,15 +28,18 @@ import risicammaraJava.turnManage.PartitaClient;
  * @author matteo
  */
 public class PlanciaImmagine extends Elemento_2DGraphicsCliccable {
+    private static final int CERCHIO = 30;
+    private static final int OFFSET_CORONA = 3;
     //private AttivatoreGrafica attivatoreGrafica;
     private BufferedImage planciaBMP;
     private BufferedImage planciaPNG;
     private BufferedImage planciaPNGfinal;
-    //private PartitaClient partita;
+    private PartitaClient partita;
     private PlanciaClient plancia;
 
     public PlanciaImmagine(Point posizione, PartitaClient partita) {
         super();
+        this.partita = partita;
         this.plancia = partita.getPlancia();
         planciaBMP = loadImage("./risorse/risicammara_plancia.bmp");
         planciaPNG = loadImage("./risorse/risicammara_plancia.png");
@@ -41,6 +48,7 @@ public class PlanciaImmagine extends Elemento_2DGraphicsCliccable {
         rettangolo.setSize(planciaPNG.getWidth(null), planciaPNG.getHeight(null));
         super.setShape(rettangolo);
 
+        
         for (territori_t territorio : territori_t.values()) {
             if (territorio == territori_t.Jolly1 || territorio == territori_t.Jolly2)
                 continue;
@@ -49,8 +57,9 @@ public class PlanciaImmagine extends Elemento_2DGraphicsCliccable {
             Point p = new Point(rect.x+(rect.width/2), rect.y+(rect.height/2));
             plancia.setBounds(territorio, rect, p);
 
-            int indexProprietario = plancia.getTerritorio(territorio).getProprietario();
-            coloraSfumato(idTerritorio, partita.getListaGiocatori().get(indexProprietario).getArmyColour().getColor(), 0.7);
+            //colora ogni territorio (in trasparenza) del colore del proprietario
+            //int indexProprietario = plancia.getTerritorio(territorio).getProprietario();
+            //coloraSfumato(idTerritorio, partita.getListaGiocatori().get(indexProprietario).getArmyColour().getColor(), 0.7);
         }
     }
 
@@ -77,11 +86,49 @@ public class PlanciaImmagine extends Elemento_2DGraphicsCliccable {
         return new Rectangle(sinistra, alto, destra-sinistra+1, basso-alto+1);
     }
 
-    public void disegna(Graphics2D graphics2D, GraphicsAdvanced graphicsAdvanced) {
+    public void disegna(Graphics2D g2, GraphicsAdvanced colori) {
         Point p = ((Rectangle)posizione).getLocation();
-        graphics2D.setColor(Color.cyan.brighter());
-        graphics2D.fill(posizione);
-        graphics2D.drawImage(planciaPNG,p.x,p.y,null);
+        g2.setColor(Color.cyan.brighter());
+        g2.fill(posizione);
+        g2.drawImage(planciaPNG,p.x,p.y,null);
+
+        FontMetrics font = g2.getFontMetrics();
+        for (TerritorioPlancia territorio : plancia.getTabellone()) {
+            disegnaSegnaposto(g2, colori, font, (TerritorioPlanciaClient) territorio);
+        }
+    }
+
+    public void disegnaSegnaposto(Graphics2D g2, GraphicsAdvanced colori, FontMetrics font, TerritorioPlanciaClient territorio){
+        Point p = territorio.getPosizioneCerchietto();
+        Colore_t coloreProprietario = partita.getListaGiocatori().get(territorio.getProprietario()).getArmyColour();
+
+        //determina la posizione del numero (delle armate) da stampare
+        String numeroArmate = ""+territorio.getArmate();
+        Rectangle posizioneTesto = new Rectangle();
+        posizioneTesto.width = font.stringWidth(numeroArmate);
+        posizioneTesto.height = font.getHeight();
+        posizioneTesto.x = p.x-(posizioneTesto.width/2);
+        posizioneTesto.y = p.y+(posizioneTesto.height/2)+PannelloGioco.ALTEZZAPANNELLO;
+        
+        //determina le dimensioni dell'ovale da disegnare
+        Rectangle dimensioniOvale = new Rectangle(p.x-(CERCHIO/2), p.y-(CERCHIO/2)+PannelloGioco.ALTEZZAPANNELLO, CERCHIO, CERCHIO);
+        if (posizioneTesto.width > dimensioniOvale.width+OFFSET_CORONA*2) {
+            dimensioniOvale.width = posizioneTesto.width+OFFSET_CORONA*2;
+            dimensioniOvale.x = p.x-(dimensioniOvale.width/2);
+        }
+        Rectangle dimensioneOvaleRidotto = new Rectangle(dimensioniOvale.width-OFFSET_CORONA*2, dimensioniOvale.height-OFFSET_CORONA*2);
+        dimensioneOvaleRidotto.x = p.x-(dimensioneOvaleRidotto.width/2);
+        dimensioneOvaleRidotto.y = p.y-(dimensioneOvaleRidotto.height/2)+PannelloGioco.ALTEZZAPANNELLO;
+
+        //disegna l'ovale
+        g2.setColor(Color.BLACK);
+        g2.fillOval(dimensioniOvale.x,dimensioniOvale.y, dimensioniOvale.width, dimensioniOvale.height);
+        g2.setColor(coloreProprietario.getColor());
+        g2.fillOval(dimensioneOvaleRidotto.x, dimensioneOvaleRidotto.y, dimensioneOvaleRidotto.width, dimensioneOvaleRidotto.height);
+
+        //disegna il testo sopra all'ovale (numero di armate)
+        g2.setColor(GraphicsAdvanced.getTextColor(coloreProprietario));
+        g2.drawString(numeroArmate, posizioneTesto.x, posizioneTesto.y);
     }
 
     public Rectangle getRettangolo(){
