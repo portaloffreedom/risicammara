c9b1862ba769a27b47275fba879fc4bc96802d8a
matteo.dek@gmail.com <matteo.dek@gmail.com@fbff0255-8af7-72cb-3705-11769a256df1>
2011-02-25 00:45:48 +0000

    (Java) - il giocatore nero adesso vede le sagome delle carte bianche

--- trunk/Risicammara-java/src/PacchettoGrafico/PannelloGiocoPackage/CartaDisegnabile.java
+++ trunk/Risicammara-java/src/PacchettoGrafico/PannelloGiocoPackage/CartaDisegnabile.java
@@ -10,6 +10,8 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import risicammaraJava.deckManage.Carta;
+import risicammaraJava.playerManage.Giocatore;
+import risicammaraClient.Colore_t;
 
 /**
  *
@@ -20,10 +22,14 @@ public class CartaDisegnabile extends SottoMenuCarta {
     private Carta carta;
     private BufferedImage tipo;
 
-    public CartaDisegnabile(Carta carta, int altezzaCarta, int larghezzaCarta) {
+    public CartaDisegnabile(Carta carta, int altezzaCarta, int larghezzaCarta, Giocatore meStesso) {
         super(carta.tipoCarta(), altezzaCarta, larghezzaCarta);
         this.carta = carta;
         tipo = carta.getIcona();
+        
+        //fai diventare l'immagine bianca per il giocatore nero
+        if (meStesso.getArmyColour() == Colore_t.NERO)
+            InvertiColori(tipo);
     }
 
     public Carta getCarta() {
@@ -41,6 +47,30 @@ public class CartaDisegnabile extends SottoMenuCarta {
         
         g2.drawImage(tipo, null, x, y);
         
+    }
+    
+    static private void InvertiColori(BufferedImage image){
+        int RGB;
+        for (int r=0; r<image.getHeight(); r++) {
+            for (int c=0; c<image.getWidth(); c++) {
+                RGB = image.getRGB(c, r);
+                RGB = InvertiColori(RGB);
+                image.setRGB(c, r, RGB);
+            }
+        }
+    }
+    
+    static private int InvertiColori(int RGB) {
+         int Amask = 0x000000ff;    //(Alpha)
+         int Rmask = 0x0000ff00;    //(Red)
+       //int Gmask = 0x00ff0000;    //(Green)
+         int Bmask = 0xff000000;    //(Blue)
+         
+       int RGBsfumato = ~RGB;
+       RGBsfumato = (RGBsfumato & ~Bmask) | (RGB & Bmask);
+
+
+       return RGBsfumato;
     }
    
 }
